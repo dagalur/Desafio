31f1771b99f1c3d86bf2e257ce247eefa42cc64c
dagalur <ma7h3us@hotmail.com>
2024-07-28 09:19:16 -0300

    teste.py

--- Matrix/teste.py
+++ Matrix/teste.py
@@ -1,32 +1,31 @@
-n = ['7 3', 'Tsi','h%x','i #','sM','$a','#t%','ir!']
+#!/bin/python3
 
-worda = ''
-wordb = ''
-wordc = ''
-wordd = ''
-m = 0
+import math
+import os
+import random
+import re
+import sys
 
-while m < 8:
-    match m:
-        case 1:
-            worda = n[m].replace('si','')
-            wordb = n[m].replace('T','')
-        case 2:
-            worda += n[m].replace('%x','') + wordb[::-1] + ' '
-            wordc = n[m].replace('h%','')
-        case 3:
-            worda += n[m].replace(' #','')
-        case 4:
-            wordb = n[m].replace('s','')
-            worda += n[m].replace('M','') + ' ' + wordb
-        case 5:
-            worda += n[m].replace('$','')
-        case 6:
-            worda += n[m].replace('#','').replace('%','')
-            wordb = n[m].replace('t',' ')
-        case 7:
-            wordd = n[m].replace('ir', '')
-            worda += (n[m])[::-1].replace('!','') + wordc + wordb + wordd
-    m+=1
 
-print('\n' + worda)
+
+
+first_multiple_input = input().rstrip().split()
+
+n = int(first_multiple_input[0])
+
+m = int(first_multiple_input[1])
+
+matrix = []
+
+for _ in range(n):
+    matrix_item = input()
+    matrix.append(matrix_item)
+
+recebe = ''
+
+converte = list(zip(*matrix))
+
+for alinha in converte:
+    recebe = recebe + ''.join(alinha)
+
+print(re.sub(r'\b[^a-zA-Z0-9]+\b', r' ', recebe))
